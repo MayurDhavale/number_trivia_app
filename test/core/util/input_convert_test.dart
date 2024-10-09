5510315568f8b4bcbc815558d62b4f8bce8caab3
Mayur Dhavale <mayurdhavale2002@gmail.com>
2024-10-09 21:51:30 +0530

    wensday

---
+++ test/core/util/input_convert_test.dart
@@ -0,0 +1,56 @@
+
+import 'package:dartz/dartz.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:number_trivia_app/core/error/failures.dart';
+import 'package:number_trivia_app/core/util/input_convert.dart';
+
+void main(){
+  late InputConverter inputConverter;
+
+  setUp(() =>{
+      inputConverter = InputConverter()
+  });
+
+  group('stringToUnsignedInt', 
+  (){
+
+      test('should return an integer when the string represents an unsigned integer', 
+      () async{
+
+        //arrange
+          final str = '1';
+
+        //act
+          final result = inputConverter.stringToUnsignedInteger(str);
+        //assert
+          expect(result,  Right(1));
+      });
+
+
+      test('should return a failure when the string is not a integer', 
+      ()async{
+
+        //arrange
+          const  str = 'abc';
+
+        //act
+          final result = inputConverter.stringToUnsignedInteger(str);
+        //assert
+          expect(result, Left(InvalidInputFailure()));
+
+      });
+
+      test('should return a failure when the string is a negative number', 
+      ()async{
+          //arrange
+            const str = '-123';
+          //act
+           final result = inputConverter.stringToUnsignedInteger(str);
+
+          //assert
+          expect(result, Left(InvalidInputFailure()));
+      }
+      );
+
+  });
+}
